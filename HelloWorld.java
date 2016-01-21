c275b71eec7c8d83ea5f0eaed15263227c179488
sakwe <sakwe@yopmail.com>
2016-01-21 13:39:48 +0100

    new files for work

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,12 @@ class HelloWorld
 {
 public static void main(String args[]){
   System.out.println("I AM HELLO WORLD");
-}
+   int sakwe;
+ if(sakwe ==null){
+     sakwe='he was empty first';
+      System.out.println(sakwe);
+  }else{
+    System.out.println('my state'+sakwe);
+   }
+
 }
